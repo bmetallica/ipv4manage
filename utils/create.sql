319ab4c7cda8afe188ac5530d7952eba12d69c95
Daniel Boden <91453533+bmetallica@users.noreply.github.com>
2024-12-09 11:56:50 +0100

    Update create.sql
    Umbau auf tab statt name

--- utils/create.sql
+++ utils/create.sql
@@ -2,5 +2,6 @@ CREATE TABLE address_spaces (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     cidr VARCHAR(20) NOT NULL,
-    scan VARCHAR(20) NOT NULL
+    scan VARCHAR(20) NOT NULL,
+    tab character varying(10) COLLATE pg_catalog."default" NOT NULL GENERATED ALWAYS AS (('ipvx'::text || id)) STORED
 );
